ae7f640ebcc456f1bad28b0b677a6bd9badc297a
rashard <rashard@udel.edu>
2026-09-20 16:25:03 -0400

    FINISHED TASK 10!!!

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,56 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
+const COLORS: string[] = [
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "cyan",
+    "magenta",
+    "white",
+    "black",
+];
 export function ChangeColor(): React.JSX.Element {
+    const [color, setColor] = useState<string>("red");
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
     return (
         <div>
             <h3>Change Color</h3>
+            {COLORS.map((col: string) => (
+                <Form.Check
+                    inline
+                    key={col}
+                    type="radio"
+                    name="colors"
+                    onChange={updateColor}
+                    id="selectColor"
+                    label={col}
+                    value={col}
+                    checked={col === color}
+                ></Form.Check>
+            ))}
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor:
+                        COLORS[
+                            COLORS.findIndex(
+                                (col: string): boolean => col === color,
+                            )
+                        ],
+                    display: "inline-block",
+                    verticalAlign: "bottom",
+                    marginLeft: "5px",
+                }}
+            >
+                The color is {color}
+            </div>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,40 @@
 import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptRequests, setAttemptRequests] = useState<string>("0");
+    const userInput: number = parseInt(attemptRequests) || 0;
+    function updateAttempts(event: React.ChangeEvent<HTMLInputElement>) {
+        setAttemptRequests(event.target.value);
+    }
     return (
         <div>
             <h3>Give Attempts</h3>
+            <h4>Remaining Attempts: {attemptsLeft}</h4>
+            <Form.Group controlId="requestingAttempts">
+                <Form.Label>Requested Attempts:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={attemptRequests}
+                    onChange={updateAttempts}
+                ></Form.Control>
+            </Form.Group>
+            <Button
+                onClick={() => {
+                    setAttemptsLeft(attemptsLeft - 1);
+                }}
+                disabled={attemptsLeft === 0}
+            >
+                use
+            </Button>
+            <Button
+                onClick={() => {
+                    setAttemptsLeft(attemptsLeft + userInput);
+                }}
+            >
+                gain
+            </Button>
         </div>
     );
 }
